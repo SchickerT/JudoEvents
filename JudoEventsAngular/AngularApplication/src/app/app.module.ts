621f531a6ccfb9c0298f894a7e7533b84209476a
Unknown <marcel.pautz@gmail.com>
2017-10-11 00:06:06 +0200

    CREATED: Header for JudoEvents and did some minor changes

--- JudoEventsAngular/AngularApplication/src/app/app.module.ts
+++ JudoEventsAngular/AngularApplication/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './app-components/navbar/navbar.component';
+import {MainpageComponent} from "./app-components/mainpage/mainpage.component";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule
