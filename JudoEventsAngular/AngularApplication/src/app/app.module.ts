f7ffd4b512b83056c28c22db2013d9d21767fc80
Unknown <marcel.pautz@gmail.com>
2017-10-12 17:22:11 +0200

    ADDED:Footer

--- JudoEventsAngular/AngularApplication/src/app/app.module.ts
+++ JudoEventsAngular/AngularApplication/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './app-components/navbar/navbar.component';
 import {MainpageComponent} from "./app-components/mainpage/mainpage.component";
+import { FooterComponent } from './app-components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MainpageComponent
+    MainpageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
